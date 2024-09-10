049ec951b3b9ba67a5bc9ddd82d6bf2ec2c94865
Depechejuan <depechejuan@gmail.com>
2024-09-10 20:57:27 +0200

    primer dia de clase

---
+++ Primero/Programacion/Workspace/Prueba 1/MyClass.cs
@@ -0,0 +1,18 @@
+namespace Test2
+{
+    public class MyClass
+    {
+        public static int AddNumbers(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static int MathOperator(int number)
+        {
+            return number * number + number - 7
+        }
+    }
+}
+
+
+// INSTALAR RIDER
---
+++ Primero/Programacion/Workspace/Prueba 1/test2.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace Test2
+{
+    internal class Program
+    {
+        // Public, Static, Tipo, Nombre(arg1 arg2....)
+        //{
+        // ..... return
+        //}
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Bye, World!");
+            int resultado;
+            resultado = MyClass.SumarNumeros(3, 4);
+            resultado2 = MyClass.MathOperator(8);
+    }
+}
+
+// Tipos
+// Void ????
+// int
+// double (es un real, = float)
+// string
+// bool
+
+// para invocar:
+// Nombre de la clase donde se encuentra la función
