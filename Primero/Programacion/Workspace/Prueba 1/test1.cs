069baff4067d923830a8784f2a5a6378719b7d45
unknown <depechejuan@gmail.com>
2024-09-12 16:20:16 +0200

    segundo dia

---
+++ Primero/Programacion/Workspace/Prueba 1/test1.cs
@@ -0,0 +1,17 @@
+using System;
+
+
+namespace Test1
+// Dentro de "namespaces" solo hay clases.
+// 1 clase por achivo
+// Se utilizan para: Crear objetos o hacer funciones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Bye, World!");
+        }
+    }
+}
