4e34f3314bc0f5018a2cccd21c439a070f4528ec
Juan León <depechejuan@gmail.com>
2025-12-09 20:41:41 +0100

    Actualizacion de servidor y eliminacion portfolio

---
+++ Segundo/ENT.CLIENTE/Ejercicio 5/chistes.js
@@ -0,0 +1,50 @@
+let chistes = [
+    {
+	enunciado: "¿Qué le dice un bit a otro?",
+	respuesta: "Nos vemos en el bus."
+    },
+    {
+	enunciado: "¿Qué es un terapeuta?",
+	respuesta: "1024 GigaPeutas."
+    },
+    {
+    enunciado: "¿Qué son 8 bocabits?",
+	respuesta: "1 bocabyte"   
+    },
+    {
+	enunciado: "¿Cuántos programadores hacen falta para cambiar una bombilla?",
+	respuesta: "Ninguno, porque es un problema de hardware."
+    }
+]
+
+
+
+class MiElemento extends HTMLElement {
+    constructor() {
+        super()
+    }
+
+    connectedCallback() {
+        const shadow = this.attachShadow({mode: "open"})
+        const tpl = document.getElementById("tpl")
+        const clon = tpl.content.cloneNode(true)
+        let enunciado = clon.querySelector(".chiste")
+        enunciado.textContent = this.enunciado;
+        let btn = clon.querySelector("button")
+
+        btn.addEventListener("click", (e) => {
+            alert(this.punchline)
+        })
+        shadow.append(clon)
+    }
+}
+
+customElements.define("mi-elemento", MiElemento)
+
+for(let chiste of chistes) {
+    let joke = document.createElement("mi-elemento")
+    joke.enunciado = chiste.enunciado
+    joke.punchline = chiste.respuesta
+    let div = document.getElementById("listado")
+    div.append(joke)
+}
